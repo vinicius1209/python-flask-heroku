760ae4e7a0a0b205f160138fd4e2fc1c5aa82f2d
Vinicius Machado <viniciusadrianomachado@gmail.com>
2018-12-10 12:56:11 -0200

    Task table setup

---
+++ app/models.py
@@ -0,0 +1,8 @@
+from app import db
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.Text(), nullable=False)
+
+    def __repr__(self):
+        return '<Task: {}>'.format(self.name)
--- config.py
+++ config.py
@@ -3,5 +3,5 @@ from app import dashboard
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'cout << "Vinicius" << endl;'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(dashboard.instance_path, 'task_list.sqlite'),
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///app.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
