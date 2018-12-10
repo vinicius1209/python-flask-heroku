862b7e5b4d55936ffbc08953b920482a49d5f9a6
Vinicius Machado <viniciusadrianomachado@gmail.com>
2018-12-10 12:42:58 -0200

    Configuracao do database

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,17 @@
 from flask import Flask
-from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 dashboard = Flask(__name__)
+
+from config import Config
+
 dashboard.config.from_object(Config)
+db = SQLAlchemy(dashboard)
+migrate = Migrate(dashboard, db)
+
+db.init_app(dashboard)
+migrate.init_app(dashboard, db)
 
 from app import routes
 
--- config.py
+++ config.py
@@ -1,4 +1,7 @@
 import os
+from app import dashboard
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'cout << "Vinicius" << endl;'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(dashboard.instance_path, 'task_list.sqlite'),
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
