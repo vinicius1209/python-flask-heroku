159e76f8c59f8435b283118735e656197e677878
Vinicius Machado <viniciusadrianomachado@gmail.com>
2018-12-10 14:54:32 -0200

    Tentando resolver o Procfile

--- config.py
+++
@@ -1,7 +0,0 @@
-import os
-from task_list import dashboard
-
-class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'cout << "Vinicius" << endl;'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(dashboard.instance_path, 'task_list.sqlite')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
